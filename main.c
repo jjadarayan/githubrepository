f06ce9dc70821b8262f4e4882adf30e041ace5fc
adarayanj <adarayanj@amerigas.com>
2019-11-05 10:27:04 -0500

    Give a personalized hello.

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 
 int main(int argc, char **argv)
 {
-	printf("Hello world!\n");
+	char name[255];
+	
+	printf("Enter your name: ");
+	fgets(name, 255, stdin);
+	
+	name[strlen(name)-1] = '\0\;
+	
+		
+	printf("Hello world!\n", name);
 	return 0;
 }
